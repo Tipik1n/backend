be3dbaf692168b161ff287b11b9dfd77ea170bae
DanielT <TipikinJ97@gmail.com>
2023-07-23 21:21:55 +0300

    Added jwt auth impl

--- base/api/views.py
+++ base/api/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
